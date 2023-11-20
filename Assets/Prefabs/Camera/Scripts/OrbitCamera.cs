2046ae03f9162511513395781d8477ff0d5b1aba
NexusI9 <nassim.elkhantour@gmail.com>
2023-11-20 21:44:37 +0100

    setup orbit camera

--- Assets/Prefabs/Camera/Scripts/OrbitCamera.cs
+++ Assets/Prefabs/Camera/Scripts/OrbitCamera.cs
@@ -2,88 +2,80 @@ using UnityEngine;
 
 public class OrbitCamera : MonoBehaviour
 {
-    public Transform target; // The target object to orbit around
-    public float rotationSpeed = 3f; // Rotation speed with ZQSD keys
-    public float zoomSpeed = 3f; // Zoom speed with mouse scroll
-    public float minZoom = 5f; // Minimum zoom distance
-    public float maxZoom = 20f; // Maximum zoom distance
-    public bool preventBelowFloor = true; // Toggle to prevent camera from going below the floor
-    private float floorLevel = 0f; // Adjust this if your floor is at a different height
+    public float moveSpeed = 5f;
+    public float zoomSpeed = 5f;
+    public float rotationSpeed = 2f;
+    public bool restrictBelowFloor = true;
+    private bool isOrbiting = false;
 
-    void Update()
+    private void Update()
     {
+        // Move the camera
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        MoveCamera(horizontal, vertical);
 
-        CenterCameraOnTarget();
+        // Zoom the camera
+        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        ZoomCamera(scrollWheel);
 
-        // Orbit around the target with ZQSD keys
-        OrbitWithKeys();
-
-        // Zoom in/out with the mouse scroll wheel
-        ZoomWithMouseScroll();
-
-        // Optionally prevent camera from going below the floor
-        if (preventBelowFloor && target != null)
+        // Check for right mouse button input
+        if (Input.GetMouseButtonDown(1))
         {
-            PreventBelowFloor();
+            isOrbiting = true;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isOrbiting = false;
+        }
+
+        // Orbit around the floor using horizontal scroll only when right mouse button is pressed
+        if (isOrbiting)
+        {
+            Orbit();
         }
     }
 
-    private void CenterCameraOnTarget()
+    private void MoveCamera(float horizontal, float vertical)
     {
-        if (target != null)
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void ZoomCamera(float scrollWheel)
+    {
+        transform.Translate(Vector3.forward * scrollWheel * zoomSpeed, Space.Self);
+    }
+
+    private void Orbit()
+    {
+
+        // Cast a ray from the camera to the floor
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
         {
-            // Center the camera's X and Z coordinates on the target
-            transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+            // Get the point on the floor where the ray hits
+            Vector3 hitPoint = hit.point;
+
+            // Orbit the camera around the hit point
+            transform.RotateAround(hitPoint, Vector3.up, Input.GetAxis("Mouse X") * rotationSpeed);
         }
     }
 
-    private void OrbitWithKeys()
+    private void LateUpdate()
     {
-        if (target == null)
+        // Restrict camera below the floor if enabled
+        if (restrictBelowFloor)
         {
-            return;
-        }
-
-        // Rotate around the target using ZQSD keys
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        float rotationAngle = rotationSpeed * Time.deltaTime;
-        transform.RotateAround(target.position, Vector3.up, horizontalInput * rotationAngle);
-        transform.RotateAround(target.position, transform.right, verticalInput * rotationAngle);
-    }
-
-    private void ZoomWithMouseScroll()
-    {
-        // Zoom in/out with the mouse scroll wheel
-        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-        float zoomAmount = scrollInput * zoomSpeed * Time.deltaTime;
-
-        // Clamp the zoom distance between minZoom and maxZoom
-        float newZoom = Mathf.Clamp(transform.position.y - zoomAmount, minZoom, maxZoom);
-
-        // Apply the new zoom distance
-        transform.position = new Vector3(transform.position.x, newZoom, transform.position.z);
-    }
-
-    private void PreventBelowFloor()
-    {
-        // Optionally prevent the camera from going below the floor, considering camera rotation
-        Vector3 cameraDirection = transform.forward;
-
-        // Check if the camera is facing downward
-        if (Vector3.Dot(cameraDirection, Vector3.up) > 0)
-        {
-            // Calculate the intersection point of the camera's view direction with the floor
-            Vector3 intersectionPoint = transform.position - cameraDirection * (transform.position.y - floorLevel) / cameraDirection.y;
-
-            // Adjust the camera's position if it is below the floor
-            if (intersectionPoint.y < floorLevel)
-            {
-                transform.position += Vector3.up * (floorLevel - intersectionPoint.y);
-            }
+            RestrictBelowFloor();
         }
     }
 
-
+    private void RestrictBelowFloor()
+    {
+        float yPos = Mathf.Max(0f, transform.position.y);
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+    }
 }
